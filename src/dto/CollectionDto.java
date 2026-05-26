fd406f1d01b62eed71cf722b63734070e1bf1dab
gusqkr <hyeon05@ewha.ac.kr>
2026-05-26 19:36:42 +0900

    feat: CollectionDto 속성 정의, 전 속성 포함 생성자, getter, setter 작성

--- src/dto/CollectionDto.java
+++ src/dto/CollectionDto.java
@@ -1,4 +1,43 @@
 package dto;
 
 public class CollectionDto {
+    // 도서 상태 정의
+    public enum BookStatus {
+        AVAILABLE, BORROWED, RESERVED
+    };
+
+    private int book_id;
+    private int library_id;
+    private BookStatus status;
+
+    // 모든 속성을 포함하는 생성자
+    public CollectionDto(int book_id, int library_id, BookStatus status) {
+        this.book_id = book_id;
+        this.library_id = library_id;
+        this.status = status;
+    }
+
+    public int getBook_id() {
+        return book_id;
+    }
+
+    public void setBook_id(int book_id) {
+        this.book_id = book_id;
+    }
+
+    public int getLibrary_id() {
+        return library_id;
+    }
+
+    public void setLibrary_id(int library_id) {
+        this.library_id = library_id;
+    }
+
+    public BookStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(BookStatus status) {
+        this.status = status;
+    }
 }
