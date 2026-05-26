8eeb94488519131063360e0bd4e4ab5a22acd21e
gusqkr <hyeon05@ewha.ac.kr>
2026-05-27 01:49:51 +0900

    feat: 일반 대출하기 기능에 사용되는 메서드 각 repository에서 정의

--- src/repository/LoanRecordRepository.java
+++ src/repository/LoanRecordRepository.java
@@ -1,5 +1,8 @@
 package repository;
 import dto.LoanRecordDto;
+import java.sql.Connection;
 
 public interface LoanRecordRepository {
+    // 대출 기록 데이터 삽입 - 일반 대출
+    int insertLoanRecord(Connection conn, LoanRecordDto loanRecordDto);
 }
--- src/repository/UserRepository.java
+++ src/repository/UserRepository.java
@@ -1,5 +1,9 @@
 package repository;
-import dto.UserDto;
+import java.sql.Connection;
 
 public interface UserRepository {
+    // 회원 현재 대출 권수 받아오기 - 일반 대출
+    int getLoanCount(Connection conn, int user_id);
+    // 현재 대출 권수 1권 증가시키기 - 일반 대출
+    void increaseLoanCount(Connection conn, int user_id);
 }
