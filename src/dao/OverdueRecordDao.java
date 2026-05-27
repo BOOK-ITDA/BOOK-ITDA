4e66e70aa280b1c5dd4901eae3ccc1b6b5be8a89
gusqkr <hyeon05@ewha.ac.kr>
2026-05-27 11:48:38 +0900

    feat: OverdueRecordDao 도서 대출 기능에 사용될 Dao 메서드 구현

--- src/dao/OverdueRecordDao.java
+++ src/dao/OverdueRecordDao.java
@@ -1,7 +1,24 @@
 package dao;
-import dto.OverdueRecordDto;
 import repository.OverdueRecordRepository;
-import database.DatabaseConnector;
 import java.sql.*;
+
 public class OverdueRecordDao implements OverdueRecordRepository {
+    @Override
+    public boolean hasUnpaidOverdue(Connection conn, int user_id) {
+        String sql = "SELECT COUNT(*) FROM overdue_record o " +
+                "JOIN loan_record l ON o.loan_id = l.loan_id " +
+                "WHERE l.user_id = ? AND o.is_paid = 0";
+        try(PreparedStatement pstmt = conn.prepareStatement(sql)){
+            pstmt.setInt(1, user_id);
+            try(ResultSet rs = pstmt.executeQuery()){
+                if(rs.next()) {
+                    return rs.getInt(1) > 0;
+                }
+            }
+        } catch (Exception e){
+            System.out.println("DAO 에러 발생 : " + e.getMessage());
+            throw new RuntimeException("미납 연체 정보 조회 중 DB 오류 발생",e);
+        }
+        return false;
+    }
 }
