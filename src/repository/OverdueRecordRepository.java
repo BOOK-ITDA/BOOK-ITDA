2947cc16eb09e285b1c613410f1111f3b1fdd225
gusqkr <hyeon05@ewha.ac.kr>
2026-05-28 23:48:19 +0900

    feat: 연체 기록 저장, 연체료 업데이트 메서드 구현

--- src/repository/OverdueRecordRepository.java
+++ src/repository/OverdueRecordRepository.java
@@ -1,7 +1,14 @@
 package repository;
+import dto.LoanRecordDto;
+
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public interface OverdueRecordRepository {
     // 연체자인지 확인 (납부하지 않은 연체료 존재 여부) - 일반 대출
     boolean hasUnpaidOverdue(Connection conn, int user_id);
+    // 새 연체기록 저장 - 연체 처리
+    void insertOverdueRecord(Connection conn) throws SQLException;
+    // 연체료 업데이트 - 연체 처리 (하루에 한 번 실행되도록 함)
+    void updateDailyFineAmount(Connection conn) throws SQLException;
 }
