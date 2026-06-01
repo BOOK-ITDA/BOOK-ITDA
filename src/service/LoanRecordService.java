46146982e2b7e65a82e9bb0c9012e335d0aeab46
gusqkr <hyeon05@ewha.ac.kr>
2026-06-02 03:28:01 +0900

    feat: 현재 대출 중인 도서 대출 기록 조회용 메서드 생성 (LoanRecordService에 추가)

--- src/service/LoanRecordService.java
+++ src/service/LoanRecordService.java
@@ -1,5 +1,6 @@
 package service;
 
+import database.DatabaseConnector;
 import dto.LoanRecordDto;
 import repository.LoanRecordRepository;
 
@@ -16,5 +17,13 @@ public class LoanRecordService {
 
     public void getMyLoanRecord(int userId) throws SQLException {
         loanRecordRepository.findRecord(userId);
+    }
+
+    public List<LoanRecordDto> getActiveLoans(int user_id) {
+        try (Connection conn = DatabaseConnector.getConnection()) {
+            return loanRecordRepository.findActiveByUserId(conn, user_id);
+        } catch (SQLException e) {
+            throw new RuntimeException("현재 대출 중인 목록을 불러오는 중 데이터베이스 에러 발생", e);
+        }
     }
 }
