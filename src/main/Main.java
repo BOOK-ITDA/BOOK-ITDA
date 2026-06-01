881eab9e3c9dfafba27717b07e7f13f93e91863b
girimNam <rlfla@naver.com>
2026-06-02 02:20:03 +0900

    fix: main.java 문구 변경

--- src/main/Main.java
+++ src/main/Main.java
@@ -21,7 +21,7 @@ public class Main {
         try {
             new MainUi().showMainScreen();
         } catch (Exception e) {
-            System.out.println("DB 연결 실패: " + e.getMessage());
+            System.out.println("오류 발생: " + e.getMessage());
         }
     }
 }
