3b4d7362e9247cbe86eeff71cfc64b85df5ec7c1
HSYoon <yhs040421@ewha.ac.kr>
2026-06-02 22:51:04 +0900

    feat: isStaff 삭제

--- src/session/Session.java
+++ src/session/Session.java
@@ -3,29 +3,23 @@ package session;
 public class Session {
     //계속 가지고 있을 로그인 후 회원 아이디
     private static int loggedInUserId = -1;
-    // 사서 여부
-    private static boolean isStaff = false;
+
     //로그인 성공하면 아이디 번호 업데이트
     public static void loginAsUser(int userId) {
         loggedInUserId = userId;
-        isStaff = false;
     }
 
     // 사서 로그인
     public static void loginAsStaff() {
-        loggedInUserId = -1;
-        isStaff = true;
     }
 
-    // 로그아웃
+    // 회원 로그아웃
     public static void logout() {
         loggedInUserId = -1;
-        isStaff = false;
     }
 
     //나중에 다른 기능에서 필요할 때 Sesson.getUserId()로 이용하면 됨
     public static int getUserId() { return loggedInUserId; }
     public static boolean isLoggedIn() { return loggedInUserId != -1; }
-    public static boolean isStaff()    { return isStaff; }
 
 }
