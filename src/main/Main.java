0ddd35abafc6b76f8dcdfedc7fdc9481a87dbe64
gusqkr <hyeon05@ewha.ac.kr>
2026-06-04 04:52:34 +0900

    feat: AppInitializer 파일 추가 (연체 처리 시작, 트리거 생성)

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,7 @@ package main;
 
 //일단 지금은 jdbc 연결하고 연결상태 확인하는 코드 넣어놨습니당.
 
+import config.AppInitializer;
 import dao.OverdueRecordDao;
 import dao.UserDao;
 import database.DatabaseConnector;
@@ -19,16 +20,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        OverdueRecordRepository overdueRepo = new OverdueRecordDao();
-        OverdueService overdueService = new OverdueService(overdueRepo);
-        overdueService.startDailyOverdueBatch(); // 자정마다 연체 기록 갱신
-
-        // 전화번호 중복 확인 트리거 생성
-        try (Connection conn = DatabaseConnector.getConnection()){
-            UserDao userDao = new UserDao();
-            userDao.createTriggerIfNotExists(conn);
-        } catch (SQLException e) {
-            System.out.println("\nDB 연결 실패로 트리거를 생성하지 못했습니다.");
-        }
+        AppInitializer.init();
     }
 }
