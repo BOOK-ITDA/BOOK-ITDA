0887bfe575b816fbd741f2d70edce0dd583f13a1
gusqkr <hyeon05@ewha.ac.kr>
2026-06-07 23:45:01 +0900

    refactor: LibraryUi 메뉴 선택 후 재귀 호출문 -> while문으로 수정

--- src/ui/LibraryUi.java
+++ src/ui/LibraryUi.java
@@ -15,9 +15,10 @@ import java.util.Scanner;
 public class LibraryUi {
     private final Scanner scanner = new Scanner(System.in);
 
-    int userId = Session.getUserId(); //회원 아이디 가져오는 부분이에용 -> 각자 유저 아이디 인풋으로 필요하시면 넣으면 됩니당.
-
     public void showLibraryScreen(){
+        int userId = Session.getUserId(); //회원 아이디 가져오는 부분이에용 -> 각자 유저 아이디 인풋으로 필요하시면 넣으면 됩니당.
+
+        while (true) {
         printLibraryMenu();
         System.out.print("메뉴 선택 : ");
         int choice = scanner.nextInt();
@@ -30,30 +31,24 @@ public class LibraryUi {
             case 1:
                 System.out.println("도서 조회 기능을 선택하셨습니다.");
                 new SearchUi().showSearchScreen();
-                showLibraryScreen();
                 break;
             case 2:
                 System.out.println("도서 반납 기능을 선택하셨습니다.");
                 new ReturnUi().showReturnBookScreen(userId);
-                showLibraryScreen();
                 break;
             case 3:
                 System.out.println("도서 연장 기능을 선택하셨습니다.");
                 ExtendUi extendUi = new ExtendUi(new ExtendService(new CollectionDao(), new OverdueRecordDao(), new LoanRecordDao()));
                 extendUi.showExtendScreen(userId);
-                showLibraryScreen();
                 break;
             case 4:
                 System.out.println("대시보드 기능을 선택하셨습니다.");
                 new DashBoardUi(userId).showDashBoardScreen();
-                showLibraryScreen();
                 break;
             default :
                 System.out.println("없는 메뉴입니다. 다시 선택해 주세요.");
-                showLibraryScreen();
-                return;
-
-
+                break;
+        }
         }
 
     }
